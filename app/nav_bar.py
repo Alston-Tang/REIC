1418fc092ee8c4798f5eb295489dcf4787291d04
tang <tanghaomo@126.com>
2015-01-10 12:22:18 +0800

    Fix bugs at section editor nav bar Fix bugs at section editor section information

--- app/nav_bar.py
+++ app/nav_bar.py
@@ -23,8 +23,16 @@ class NavBar:
                       "data": {
                           "Text": {"class": "editor-nav add-text", "add": "text"},
                           "Image": {"class": "editor-nav add-image", "add": "img"},
-                          "Picture Wall": {"class": "editor-nav add-picture-wall", "add": "picture-wall"}
-                      }}}
+                          "Picture Wall": {"class": "editor-nav add-picture-wall", "add": "picture-wall"},
+                          "Carousel": {"class": "editor-nav add-carousel", "add": "bootstrapCarousel"},
+                          "Jumbotron": {"class": "editor-nav add-jumbotron", "add": "bootstrapJumbotron"}
+
+                      }},
+              "Setting": {"_type": "drop_down",
+                          "data": {
+                              "Section Setting": {"class": "setting-nav setting-section"},
+                              "Section Information": {"class": "setting-nav setting-information"}}}
+        }
         return rv
 
     @staticmethod
